ce8aa6e0f8fa85f97aa9564cfdd7f521ead9fdbe
ecourse <smkang@codenuri.co.kr>
2025-02-17 10:20:49 +0900

    Add files via upload

---
+++ DAY1/01_namespace3.cpp
@@ -0,0 +1,18 @@
+﻿// 1_namespace3.cpp - 9page 내용
+// 
+#include <algorithm>
+
+// #1. C++의 모든 표준은 std 라는 namespace 안에 있습니다.
+// #2. 초급 시절에 "std" 를 붙이는 것이 싫어서 아래 처럼 하곤합니다.
+using namespace std; // 코드 전체에서 std:: 없이 표준함수 사용가능
+					 // 그런데, 되도록 사용하지 마세요
+
+int count = 0;
+
+int main()
+{
+	int n = std::min(3, 2); // C++ 표준 함수. algorithm 헤더 필요
+
+	count = 10; // error.. 왜 ??? 에러일까요 ?
+				// 내가 만든 전역변수인데 ??? 
+}
