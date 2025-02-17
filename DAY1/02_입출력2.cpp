10d58b3c262b4ee2397134f5f48e3f673fab22c3
ecourse <smkang@codenuri.co.kr>
2025-02-17 10:43:40 +0900

    Add files via upload

---
+++ DAY1/02_입출력2.cpp
@@ -0,0 +1,21 @@
+﻿// 3_입출력2.cpp - 18 page
+#include <iostream>
+
+// io manipulator
+// => std::cout, std::cin 사용시 입출력 서식을 지정하는 도구
+// => 대부분 <iomanip> 헤더 있습니다
+
+#include <iomanip>
+
+int main()
+{
+	int n = 10;
+	std::cout << n << std::endl; // 10 
+
+	std::cout << std::hex << n << std::endl; // a, 즉 16진수로 출력
+	std::cout << std::dec << n << std::endl; // 10, 10진수
+
+	std::cout << std::setw(10) << n << std::endl; // 
+
+	std::cout << std::setw(10) << std::setfill('#') << n << std::endl; // 
+}
