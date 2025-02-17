d36669359de1b1fd170b106dd6c594d22907217e
ecourse <smkang@codenuri.co.kr>
2025-02-17 10:53:08 +0900

    Add files via upload

--- DAY1/02_입출력2.cpp
+++ DAY1/02_입출력2.cpp
@@ -18,4 +18,29 @@ int main()
 	std::cout << std::setw(10) << n << std::endl; // 
 
 	std::cout << std::setw(10) << std::setfill('#') << n << std::endl; // 
+
+
+	double d = 12.123456789;
+
+	// std::cout 의 실수 출력 : 정수부와 실수부를 합해서 6자리까지만 출력
+	std::cout << d << std::endl;	// 12.1235
+
+	// 실수출력의 정밀도를 높이 려면 해당하는 "iomanipator" 를 사용해야 합니다.
+	// 1. cppreference.com 에서
+	// 2. std::setw() 검색 => 오른쪽 상단 검색 창
+	// 3. 상위 문서로 이동후
+	// 4. 표에서 정밀도를 높이는 iomanipator 검색
+
+	
+	std::cout << std::setprecision(12) << d << std::endl;
+
 }
+
+// 늦게 접속하신분은 
+// github.com/codenuri/kea 에서 DAY1.zip 받으시면 됩니다.
+
+// 그런데, 입출력을 많이 하게 되면
+// std::cout 이 printf 보다 많이 느립니다. 
+// 그래서, 실전에서는 printf 사용하는 분들도 많이 있습니다.
+
+// 그런데, C++23 에서 새로운 출력도 나옵니다. "std::println"
