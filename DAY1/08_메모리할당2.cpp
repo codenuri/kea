c9a5627cb104bc415162ed43a967d89f997c48d3
ecourse <smkang@codenuri.co.kr>
2025-02-17 15:31:29 +0900

    Add files via upload

--- DAY1/08_메모리할당2.cpp
+++ DAY1/08_메모리할당2.cpp
@@ -16,22 +16,26 @@ int main()
 	int* score = (int*)malloc(sizeof(int) * cnt);
 	free(score);
 
-
 	// #3. C++ 에서는 malloc 대신 new 권장
-
 	int* p1 = new int;
 	delete p1;
-
 
 	int* p2 = new int[cnt];	// 배열로 할당, 변수도 가능
 	delete[] p2;			// 주의 배열 할당은 delete 가 아닌 delete[] 사용
 }
 
 //					malloc					new
-// 정체
-// 헤더파일
-// 해지방법
-// 인자
-// 반환타입
-// 결정적차이
+// 정체				함수						연산자(컴파일러가 인식하는 키워드)
+// 헤더파일			<stdlib.h>				헤더 필요 없음.
+
+// 해지방법			free					delete 또는 delete[]
+//											배열로 할당한 경우 반드시 delete[]						
+
+// 인자				필요한 메모리 크기(숫자)	크기가 아닌 타입 전달
+
+// 반환타입			void*					인자로 전달된 타입의 * 로 반환
+//					대부분의 경우 다른타입*	캐스팅 필요없음.
+//					로 변환해서 사용
+
+// 결정적차이		생성자 호출안됨			생성자 호출됨.
 
