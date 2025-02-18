df847cbf99e409b08db1fd18bf7bbbbbe716a5f3
ecourse <smkang@codenuri.co.kr>
2025-02-18 11:37:37 +0900

    Add files via upload

--- DAY2/4_this1.cpp
+++ DAY2/4_this1.cpp
@@ -12,6 +12,8 @@ public:
 		x = a;				//		this->x = a;
 		y = b;				//		this->y = b;
 
+		// 멤버 함수 안에서 this : 현재 멤버함수를 호출할때 사용한 객체의 주소
+		std::cout << this << std::endl;
 	}
 };
 
@@ -19,9 +21,10 @@ int main()
 {
 	Point p1;
 	Point p2;
-	std::cout << "p1 : " << &p1 << std::endl;
-	std::cout << "p2 : " << &p2 << std::endl;
+	std::cout << "&p1 : " << &p1 << std::endl;
+	std::cout << "&p2 : " << &p2 << std::endl;
 
 	p1.set(10, 20);	// 이 코드는 컴파일러에 의해서 아래 처럼 변경됩니다
 					// set(&p1, 10, 20)
+	p2.set(10, 20);
 }
