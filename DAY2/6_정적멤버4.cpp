815abd9ee872e76f49e15c48ee42557355e37850
ecourse <smkang@codenuri.co.kr>
2025-02-18 14:01:29 +0900

    Add files via upload

---
+++ DAY2/6_정적멤버4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+class Car
+{
+	int color;
+	int speed;
+public:
+	static int count;
+
+	Car() { ++count; }
+	~Car() { --count; }
+};
+int Car::count = 0;
+
+
+int main()
+{
+	std::cout << Car::count << std::endl; // 0
+
+	Car c1;
+	Car c2;
+
+	std::cout << Car::count << std::endl; // 2
+
+
+}
+
