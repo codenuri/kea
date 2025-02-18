13ff596d42935436ec2f282bfe4701b30eee11c1
ecourse <smkang@codenuri.co.kr>
2025-02-18 17:27:41 +0900

    Add files via upload

---
+++ DAY2/9_도형편집기예제4.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+// 핵심 #3. 파생 클래스의 공통의 특징은 기반 클래스에도 있어야 한다.
+//	=> 문법적인 제약이 아닌
+//  => 디자인 원칙
+//  => Shape* 로 draw() 를 호출하려면 Shape 안에 draw() 있어야 한다.
+
+
+class Shape
+{
+	int color;
+public:
+	void draw() { std::cout << "draw shape\n"; }
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() { std::cout << "draw rect\n"; }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "draw circle\n"; }
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	while (true)
+	{
+		int cmd;
+
+		std::cin >> cmd;
+
+		if (cmd == 1) v.push_back(new Rect);
+		else if (cmd == 2) v.push_back(new Circle);
+		else if (cmd == 9)
+		{
+			for (auto p : v)	
+				p->draw();		
+		}
+	}
+}
+
