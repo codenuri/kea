2e163362f44fcd8672f1a18ed4f871b3126d4a88
ecourse <smkang@codenuri.co.kr>
2025-02-18 13:35:01 +0900

    Add files via upload

---
+++ DAY2/6_정적멤버3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+// 방법 #3. static member data
+
+
+
+class Car
+{
+	int color;
+	int speed;
+public:
+
+//	int count = 0;		// 객체당 한개씩 만들어 지는 멤버 데이타
+	static int count;	// 모든 Car 객체가 공유하는 멤버 데이타
+						// 1. 클래스안에 선언을 만들고
+						// 2. 클래스 외부에 정의를 만들면서 초기화 한다.
+
+	Car() { ++count; }
+	~Car() { --count; }
+};
+
+int Car::count = 0;
+
+
+int main()
+{
+	Car c1;
+	Car c2;
+		
+	// static 멤버 데이타는 "클래스이름::" 로 접근하면 됩니다.
+	std::cout << Car::count << std::endl;
+}
+
