7be9274bd8add78f9056fee541e64067ae2eb0af
ecourse <smkang@codenuri.co.kr>
2025-02-18 16:27:45 +0900

    Add files via upload

---
+++ DAY2/9_가상함수1.cpp
@@ -0,0 +1,28 @@
+﻿// 7_가상함수1  143 page ~
+#include <iostream>
+
+class Animal
+{
+public:
+	void Cry1() { std::cout << "Animal Cry1" << std::endl; } // 1
+};
+
+class Dog : public Animal
+{
+public:
+	// function override : 기반 클래스 멤버 함수를 파생 클래스가 다시 만드는 것	
+	void Cry1() { std::cout << "Dog Cry" << std::endl; }  // 2
+};
+
+int main()
+{
+	Animal a; a.Cry1(); // 1
+	Dog    d; d.Cry1(); // 2
+
+	Animal* p = &d;		
+
+	// p 자체의 타입      : Animal*
+	// p 가 가리키는 객체 : Dog
+	p->Cry1(); // 1 ? 2 
+}
+
