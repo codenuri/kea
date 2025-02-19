7f409a52e2e8a2a08b84d9650eb29335094e14d2
ecourse <smkang@codenuri.co.kr>
2025-02-19 15:26:46 +0900

    Add files via upload

--- DAY3/10_알고리즘4.cpp
+++ DAY3/10_알고리즘4.cpp
@@ -9,14 +9,16 @@ int main()
 	std::vector<int> v = { 1,2,9,4,3,6,7,3,9,10 };
 		
 	// 주어진 구간에서 3을 -1로 변경
-	std::replace(v.begin(), v.end(), 3, -1);
+//	std::replace(v.begin(), v.end(), 3, -1);
 	 
 
 	// v에서 "짝수"를 0으로 변경해 보세요 
 	// => 람다표현식을 사용해서 해보세요
 
+	std::replace_if(v.begin(), v.end(), [](int n) { return n % 2 == 0; }, 0);
+
 
 
 	for (auto e : v)
-		std::cout << e << ", ";
+		std::cout << e << ", "; // 1,0,9,0,3,0,7,3,9,0
 }
