443a4468d3deee233e237780fb087190e031466a
ecourse <smkang@codenuri.co.kr>
2025-02-19 11:25:16 +0900

    Add files via upload

--- DAY3/6_연산자재정의4.cpp
+++ DAY3/6_연산자재정의4.cpp
@@ -28,15 +28,22 @@ Point operator+(const Point& p1, const Point& p2)
 }
 
 
-
-
-
 int main()
 {
 	Point p1(1, 1);
 	Point p2(2, 2);
 
+	// 위처럼, 멤버 버전과 non-멤버 버전을 동시에 제공하면 안됩니다.
+	// 반드시 한개만 제공해야 합니다.
 	Point p3 = p1 + p2; 
 	p3.print();
+
+	// 멤버가 좋을까요 ? non-멤버가 좋을까요 ?
+	// 멤버가 좋다는 의견     : private 영역에 접근 하려면 멤버 함수가 편하다.
+	// non-멤버가 좋다는 의견 : 멤버함수로 만들수 없을때가 있다.
+
+	Point p4 = p1 + p2;
+	Point p5 = p1 + 3;
+	Point p6 = n  + p1;
 }
 
