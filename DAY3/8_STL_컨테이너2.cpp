864ddde0c3f4201607c9d5c30b9d0823f76a4883
ecourse <smkang@codenuri.co.kr>
2025-02-19 13:29:49 +0900

    Add files via upload

--- DAY3/8_STL_컨테이너2.cpp
+++ DAY3/8_STL_컨테이너2.cpp
@@ -35,10 +35,24 @@ int main()
 					// g++ : new_capacity = 기존_capacity * 2
 					// cl  : new_capacity = 기존_capacity * 1.5
 
-	std::cout << v.size() << ", " << v.capacity() << std::endl; // 4, 6
+	std::cout << v.size() << ", " << v.capacity() << std::endl; // 5, 6
+
+
+	v.clear();	// size 만 0으로 할까요 ?  ==> 0
+				// 버퍼(메모리) 자체도 제거할까요 ?
+
+	std::cout << v.size() << ", " << v.capacity() << std::endl; // 0, 6
+
+	// 메모리 자체를 제거하는 방법은 shrink_to_fit() 밖에 없습니다
+	v.shrink_to_fit(); // capacity 를 size 로 하라는 의미 인데, size == 0 이므로
+						// 메모리 제거
+
+	std::cout << v.size() << ", " << v.capacity() << std::endl; // 0, 0
 }
 
-
+// 파이썬의 list 등도 위와 유사하게 동작합니다.
+// s = [1,2,3,4]
+// s.append(0)   // 실제 메모리는 5개로 늘어나지 않고, 8개가 됩니다.
 
 
 
